e11bce1682c94c8c10686568a35738bbf5d28875
Anthony <89137593+taekwon101@users.noreply.github.com>
2022-07-05 08:48:41 -0400

    Update GuitarHeroToMIDI.ino

--- GuitarHeroToMIDI.ino
+++ GuitarHeroToMIDI.ino
@@ -1,64 +1,65 @@
 #include "MIDIUSB.h"
-#define NUM_BUTTONS  4
 
-const uint8_t button1 = 2;
-const uint8_t button2 = 3;
-const uint8_t button3 = 4;
-const uint8_t button4 = 5;
-const uint8_t button5 = 6;
-const uint8_t button6 = 7;
-const uint8_t button7 = 8;
+#define NUM_BUTTONS  10
+#define NUM_POTS 3
 
 const int ledPin =  16;// the number of the LED pin
 int ledState = LOW;
 
-const int intensityPot0 = A0;  //A0 input
-const int intensityPot1 = A1;  //A1 input
+const uint8_t fret1 = 2;
+const uint8_t fret2 = 3;
+const uint8_t fret3 = 4;
+const uint8_t fret4 = 5;
+const uint8_t fret5 = 6;
+const uint8_t fret6 = 7;
+const uint8_t fret7 = 8;
+const uint8_t fret8 = 9;
+const uint8_t fret9 = 10;
+const uint8_t fret10 = 11;
 
-const uint8_t buttons[NUM_BUTTONS] = {button1}; //, button2, button3, button4, button5, button6, button7};
+const uint8_t whammy = A0;
+const uint8_t pot1 = A1;
+const uint8_t pot2 = A2;
 
-//const byte notePitches[NUM_BUTTONS] = {48}; //, 50, 52, 53, 55, 56, 58}; //C major scale to assign to up to 7 buttons
-const byte notePitches[NUM_BUTTONS] = {52, 55, 57, 59, 62}; //E minor pentatonic to assign to up to 5 buttons
+const uint8_t frets[NUM_BUTTONS] = {fret1, fret2, fret3, fret4, fret5, fret6, fret7, fret8, fret9, fret10};
+const uint8_t pots[NUM_POTS] = {whammy, pot1, pot2};
 
+uint8_t intensity[NUM_POTS];
+uint8_t lastintensity[NUM_POTS];
+uint8_t valRaw[NUM_POTS];
+uint8_t valMap[NUM_POTS];
+uint8_t lastValMap[NUM_POTS];
+uint8_t minMap[NUM_POTS] = {21, 0, 0};
+uint8_t maxMap[NUM_POTS] = {80, 127, 127};
+
+const byte notePitches[NUM_BUTTONS] = {52, 55, 57, 59, 62, 64, 67, 69, 71, 74}; //E minor pentatonic repeated through 2 octaves to assign to up to 10 buttons
 
 uint8_t notesTime[NUM_BUTTONS];
 uint8_t pressedButtons = 0x00;
 uint8_t previousButtons = 0x00;
 
-uint8_t intensity0;
-uint8_t lastintensity0;
-uint8_t intensity1;
-uint8_t lastintensity1;
-
 int velocityDefault = 80;
+
+bool strummer = true; // permanently true until strummer sensing is implemented
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
   for (int i = 0; i < NUM_BUTTONS; i++)
-    pinMode(buttons[i], INPUT_PULLUP);
+    pinMode(frets[i], INPUT_PULLUP);
 }
 
 void loop() {
-  readButtons();
-  readIntensity();
-  if (intensity0 != lastintensity0) {
-    controlChange(1, 1, intensity0); // (channel number, control number, control value)
-    lastintensity0 = intensity0;
-  }
-  if (intensity1 != lastintensity1) {
-    controlChange(2, 2, intensity1); // (channel number, control number, control value)
-    lastintensity1 = intensity1;
-  }
+  readNotes();
+  readCC();
   playNotes();
+  MidiUSB.flush();
 }
 
-void readButtons()
+void readNotes()
 {
   for (int i = 0; i < NUM_BUTTONS; i++)
   {
-    if (digitalRead(buttons[i]) == LOW)
+    if (digitalRead(frets[i]) == LOW)
     {
       bitWrite(pressedButtons, i, 1);
       //delay(50); replace with debounce?
@@ -68,23 +69,19 @@ void readButtons()
   }
 }
 
-void readIntensity()
+void readCC() 
 {
-  int val0 = analogRead(intensityPot0);
-  intensity0 = (uint8_t) (map(val0, 21, 80, 0, 127));
-  int val1 = analogRead(intensityPot1);
-  intensity1 = (uint8_t) (map(val1, 0, 1023, 0, 127));
-}
-
-void controlChange(byte channel, byte control, byte value) 
-{
-  // First parameter is the event type (0x0B = control change).
-  // Second parameter is the event type, combined with the channel.
-  // Third parameter is the control number number (0-119).
-  // Fourth parameter is the control value (0-127).
-
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-  MidiUSB.sendMIDI(event);
+  for (int i = 0; i < NUM_POTS; i++)
+  {
+    valRaw[i] = analogRead(pots[i]);
+    valMap[i] = (byte) (map(valRaw[i], minMap[i], maxMap[i], 0, 127));
+    if (valMap[i] != lastValMap[i]) 
+    {
+      midiEventPacket_t event = {0x0B, 0xB0 | i, 1, valMap};
+      MidiUSB.sendMIDI(event);
+      valMap[i] = lastValMap[i];  
+    }
+  }
 }
 
 void playNotes()
@@ -95,25 +92,20 @@ void playNotes()
     {
       if (bitRead(pressedButtons, i))
       {
-        bitWrite(previousButtons, i , 1);
-        noteOn(0, notePitches[i], velocityDefault);
-        MidiUSB.flush();
+        if ((i <= 5) || (strummer == true))
+        {
+          bitWrite(previousButtons, i , 1);
+          noteOn(0, notePitches[i], velocityDefault);
+        }
       }
       else
       {
         bitWrite(previousButtons, i , 0);
         noteOff(0, notePitches[i], 0);
-        MidiUSB.flush();
       }
     }
   }
 }
-
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
-// Second parameter is note-on/note-off, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
@@ -123,13 +115,4 @@ void noteOn(byte channel, byte pitch, byte velocity) {
 void noteOff(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
-}
-
-void troubleshootLED(){
-  if (ledState == LOW) {
-    ledState = HIGH;
-  } else {
-    ledState = LOW;
-  }
-  digitalWrite(ledPin, ledState);
 }
